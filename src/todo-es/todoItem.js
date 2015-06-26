d6b78dcdec2551482e484052d9df266eb516c787
wangfeng <wangfeng@Ctrip.com>
2015-06-26 17:58:41 +0800

    update file

---
+++ src/todo-es/todoItem.js
@@ -0,0 +1,24 @@
+
+
+'use strict';
+
+var app = app || {};
+
+var ESCAPE_KEY = 27;
+var ENTER_KEY = 13;
+
+app.TodoItem = React.createClass({
+  handleSubmit: function (event) {
+    var val = this.state.editText.trim();
+    if (val) {
+      this.props.onSave(val);
+      this.setState({editText: val});
+    } else {
+      this.props.onDestroy();
+    }
+  },
+  handleEdit: function () {
+    this.props.onEdit();
+    this.setState({editText: this.props.todo.title});
+  }
+})
