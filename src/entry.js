5fd6926a8c100a9e8359866e34618a3895a5465b
wangfeng <wangfeng@Ctrip.com>
2015-06-29 17:01:27 +0800

    update files

--- src/entry.js
+++ src/entry.js
@@ -1,6 +1,12 @@
 
-import utils from './todo-es/Utils.js'
+// import utils from './todo-es/Utils.js'
 
-var Utils = new utils();
-var test = Utils.uuid();
-console.log("uuid:"+test);
+// var Utils = new utils();
+// var test = Utils.uuid();
+// console.log("uuid:"+test);
+
+'use strict';
+var app = app || {};
+var TodoModel;
+import './todo-es/todoModel.js';
+import './todo-es/todoApp.js';
